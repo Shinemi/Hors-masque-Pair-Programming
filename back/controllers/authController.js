b03475be3dc2d56f3903d05ed5079401e9132ef2
Shinemi <simon.lucas.83136@gmail.com>
2026-09-24 15:03:13 +0200

    feat: enhance profile update functionality with password validation and authentication middleware

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -93,7 +93,47 @@ const login = async (req, res) => {
 const getProfile = async (req, res) => {
     try {
        
+        const { name, password } = req.body
+        // utilisateur déjà authentifié (middleware)
+        const user = await User.findById(req.user._id)
 
+        if (!user) {
+            return res.status(404).json({ message: 'user not found' })
+        }
+
+        if (name) {
+            user.name = name
+        }
+
+        if (password) {
+            const isPasswordOK = validator.isStrongPassword(password, {
+                minLength: 6,
+                minLowercase: 1,
+                minUppercase: 1,
+                minNumbers: 1,
+                minSymbols: 1
+            })
+
+            if (!isPasswordOK) {
+                return res.status(400).json({ message: 'le mdp doit contenir... (tout le tralala)' })
+            }
+
+            user.password = password // sera hashé automatiquement par le hook pre('save')
+        }
+
+        // note : email et role ne sont pas modifiables ici
+
+        await user.save()
+
+        res.status(200).json({
+            message: 'profile updated successfully',
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                role: user.role,
+            }
+        })
 
     } catch (err) {
         res.status(500).json({ message: 'Server error while getting profile information', error: err.message })
--- back/routes/authRoutes.js
+++ back/routes/authRoutes.js
@@ -6,7 +6,7 @@ const authMiddleware = require('../middlewares/authMiddleware')
 
 router.post('/register', register)
 router.post('/login', login)
-router.get('/me', getProfile)
+router.get('/me', authMiddleware, getProfile)
 
 
 module.exports = router
